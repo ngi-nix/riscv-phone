0894a1e7664504312a9cdfc826eef89030aaaa1b
Uros Majstorovic <majstor@majstor.org>
2020-08-05 02:52:42 +0200

    new directory sructure for fe310 fw

--- code/fe310/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export RISCV_HOME=/opt/my/riscv/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-apple-darwin
-export RISCV_OPENOCD_HOME=/opt/my/riscv/riscv-openocd-0.10.0-2019.08.2-x86_64-apple-darwin
-export FE310_HOME=/Volumes/ecp/repo/code/fe310
