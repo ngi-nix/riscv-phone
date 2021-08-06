0102ab2bf16e1f8e8d2b46295d7acdc7f98eeaa7
Uros Majstorovic <majstor@majstor.org>
2021-08-07 01:37:23 +0200

    fixed u-boot

--- recipes-bsp/u-boot/rvphone/cl-imx8/include/configs/cl-imx8.h
+++ recipes-bsp/u-boot/rvphone/cl-imx8/include/configs/cl-imx8.h
@@ -119,11 +119,11 @@
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
 	"boot_fdt=try\0" \
-	"fdt_file="CONFIG_DEFAULT_DTB"\0" \
+	"fdt_file="CONFIG_DEFAULT_FDT_FILE"\0" \
 	"initrd_addr=0x43800000\0"		\
 	"initrd_high=0xffffffffffffffff\0" \
 	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
+	"mmcpart="__stringify(CONFIG_SYS_MMC_IMG_LOAD_PART)"\0" \
 	"mmcautodetect=yes\0" \
 	"mmcargs=setenv bootargs console=${console} root=/dev/mmcblk${mmcdev}p2 rootwait rw\0 " \
 	"loadbootscript=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
