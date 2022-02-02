dbb4fea690017cb9735ca4a6802f8f2fa3a8e951
Uros Majstorovic <majstor@majstor.org>
2022-02-02 07:36:56 +0100

    fsfat moved to ext dir

--- fw/fe310/common.mk
+++ fw/fe310/common.mk
@@ -1,7 +1,7 @@
 fe310_dir := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 bsp_dir := $(abspath $(fe310_dir)/bsp)
 eos_dir := $(abspath $(fe310_dir)/eos)
-crypto_dir := $(abspath $(fe310_dir)/../../ext/crypto)
+ext_dir := $(abspath $(fe310_dir)/../../ext)
 
 include $(fe310_dir)/platform.mk
 CFLAGS += -I$(eos_dir)
