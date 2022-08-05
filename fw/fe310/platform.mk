13c67e4c32ccf63ec48f0052fc16cee9f728e0b6
pierrebonbon <info@nunopedrosa.net>
2022-08-05 22:52:49 +0100

    reimplemented flag -specs=nano.specs at CFLAGS, fixes #3.

--- fw/fe310/platform.mk
+++ fw/fe310/platform.mk
@@ -2,4 +2,4 @@ CC =     riscv32-none-elf-gcc
 AR =     riscv32-none-elf-ar
 RANLIB = riscv32-none-elf-ranlib
 
-CFLAGS = -march=rv32imac -mabi=ilp32 -mcmodel=medlow -ffunction-sections -fdata-sections -O2
+CFLAGS = -march=rv32imac -mabi=ilp32 -mcmodel=medlow -ffunction-sections -fdata-sections --specs=nano.specs -O2
