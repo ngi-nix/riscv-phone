7c02883f58d9c53a80bfd590838a3a01bd5ebdb7
pierrebonbon <info@nunopedrosa.net>
2022-08-17 16:51:17 +0100

    move shell hook script to the let section

--- flake.nix
+++ flake.nix
@@ -6,6 +6,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        nanolibsPatch = ''
+          rm -fr nanolibs/*.a
+          mkdir -p nanolibs
+          for file in ${riscv-toolchain.newlib-nano}/riscv32-none-elf/lib/*.a; do
+             ln -s $file nanolibs
+          done
+          for file in nanolibs/*.a; do
+             mv "$file" "''${file%%.a}_nano.a"
+          done
+        '';
+
         riscv-toolchain =
           import nixpkgs {
             localSystem = "${system}";
@@ -46,6 +58,7 @@
                 done
               #'';
 
+              patchPhase = nanolibsPatch;
               checkPhase = ''
                 make -C fw/fe310/test
               '';              
