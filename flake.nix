4f04839408b5650ccac1ac3ab5ca817ed90a0f2f
pierrebonbon <info@nunopedrosa.net>
2022-08-17 16:57:07 +0100

    remove postInstall and patchPhase attributes

--- flake.nix
+++ flake.nix
@@ -48,20 +48,10 @@
                 cp -r fw/fe310/libeos.a $out
               '';
 
-              postInstall = ''
-                mkdir -p $out
-                for file in ${riscv-toolchain.newlib-nano}/riscv32-none-elf/lib/*.a; do
-                  ln -s $file $out
-                done
-                for file in $out/*.a; do
-                  mv "$file" "''${file%%.a}_nano.a"                
-                done
-              #'';
-
-              patchPhase = nanolibsPatch;
               checkPhase = ''
                 make -C fw/fe310/test
-              '';              
+              '';
+
             };
             
           };
