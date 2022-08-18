9c4133559abecbf02cd1599d145f9089991b53fa
pierrebonbon <info@nunopedrosa.net>
2022-08-19 00:09:04 +0100

    write script to the store and make executable.

--- flake.nix
+++ flake.nix
@@ -7,17 +7,19 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        nanolibsPatch = ''
-          rm -fr nanolibs/*.a
-          mkdir -p nanolibs
-          for file in ${riscv-toolchain.newlib-nano}/riscv32-none-elf/lib/*.a; do
-             ln -s $file nanolibs
-          done
-          for file in nanolibs/*.a; do
-             mv "$file" "''${file%%.a}_nano.a"
-          done
-        '';
-
+        nanolibs-script = {       
+          name = "nanolibs-path";
+          script = pkgs.writeShellScriptBin nanolibs-script.name ''
+            rm -fr nanolibs/*.a
+            mkdir -p nanolibs
+            for file in ${riscv-toolchain.newlib-nano}/riscv32-none-elf/lib/*.a; do
+               ln -s $file nanolibs
+            done
+            for file in nanolibs/*.a; do
+               mv "$file" "''${file%%.a}_nano.a"                
+            done
+          '';
+        };
         riscv-toolchain =
           import nixpkgs {
             localSystem = "${system}";
