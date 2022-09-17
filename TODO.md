ce5f88ee37ad7430656ee7443cea9a29e4a07b63
nuno <info@nunopedrosa.net>
2022-09-17 21:38:41 +0100

    Squashed commit of the following:
    commit 19e87a01ec48e02937d59885266060acd092e773
    Author: nuno <info@nunopedrosa.net>
    Date:   Sat Sep 17 21:37:46 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:flake.nix
    
    commit 80325e23ad527fe4a7750462337f01a8b16d4bc3
    Author: nuno <info@nunopedrosa.net>
    Date:   Sat Sep 17 19:16:46 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:fw/esp32/sdkconfig
    
    commit 02447227aa2518713c89cd95b5f931c8a0aa15a1
    Author: Ctem <c@ctem.me>
    Date:   Wed Sep 14 20:55:16 2022 +0900
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TODO.md
    
    commit 505cd0880dce29b024822ac4d0aac2924cd59564
    Author: a-kenji <aks.kenji@protonmail.com>
    Date:   Wed Sep 14 13:38:27 2022 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:flake.nix
    
    commit 0befb724174d2ab95f2a1353e2b8f0556941e7c4
    Author: a-kenji <aks.kenji@protonmail.com>
    Date:   Wed Sep 14 13:33:28 2022 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:flake.nix
    
    commit 704cca4b529329ca71a8bf86c154c51d21721d1a
    Author: a-kenji <aks.kenji@protonmail.com>
    Date:   Wed Sep 14 13:13:30 2022 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:flake.nix
    
    commit 4590346f15bbda01f603c0656b45581ee349dba0
    Author: a-kenji <aks.kenji@protonmail.com>
    Date:   Wed Sep 14 13:06:00 2022 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TODO.md
    
    Co-authored-by: Ctem <c@ctem.me>
    Co-authored-by: a-kenji <aks.kenji@protonmail.com>

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Todo
+
+- maybe only devshell makes sense
+-
+
+# Links
+
+https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/index.html#step-5-start-a-project
+
+```sh
+idf.py set-target esp32
+idf.py menuconfig
+```
--- flake.nix
+++ flake.nix
@@ -1,19 +1,25 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/c11d08f02390aab49e7c22e6d0ea9b176394d961";
-  inputs.nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+  };
   
   outputs = { self, nixpkgs, nixpkgs-esp-dev }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs { inherit system; overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ]; };
-  
+
       esp32-toolchain = pkgs.stdenv.mkDerivation {
         name = "riscv-esp32-firmware";
         src = ./.;
         nativeBuildInputs = with pkgs; [
-          gcc-riscv32-esp32-elf-bin
+          # commented for now
+          # gcc-riscv32-esp32-elf-bin
           esp-idf
+          gcc-xtensa-esp32-elf-bin
+          openocd-esp32-bin
+
         ];
         buildInputs = with pkgs; [
           esptool
@@ -32,24 +38,27 @@
           export IDF_PATH=$(pwd)/esp-idf
         '';
       };
-      
+
     in
     {
 
       packages.x86_64-linux.default = esp32-toolchain;
 
       devShells = {
-        x86_64-linux.default = pkgs.mkShell {
-          shellHook = ''
-          echo use
-          echo "nix develop .#esp32"
-          echo or
-          echo "nix develop .#fe310"
-          exit
-          '';
-        };
-        # usage: nix develop .#esp32
         x86_64-linux = {
+          default = pkgs.mkShell {
+            shellHook = ''
+              echo -e "use 'nix develop .#esp32'\nor 'nix develop .#fe310'."
+              exit
+            '';
+          };
+          # usage: nix develop .#mirrexagon
+          mirrexagon = nixpkgs-esp-dev.devShells.x86_64-linux.esp32-idf {
+            shellHook = ''
+              export IDF_PATH=$(pwd)/esp-idf              
+            '';
+          };
+          # usage: nix develop .#esp32
           esp32 = pkgs.mkShell {
             buildInputs = [
               esp32-toolchain
