3d669bd8b06e63ced9dde63324a6a6609a5a227f
Bill Ewanick <bill@ewanick.com>
2022-09-19 09:34:53 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    # nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/c11d08f02390aab49e7c22e6d0ea9b176394d961";
     nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
     riscvphone-src = {
       url = "git://majstor.org/rvPhone.git";
@@ -48,18 +49,23 @@
 
       packages.x86_64-linux.default = esp32-toolchain;
 
-      packages.x86_64-linux.esp32 = pkgs.callPackage pkgs.stdenv.mkDerivation {
+      packages.x86_64-linux.esp32 = pkgs.stdenv.mkDerivation {
         name = "esp32";
-        src = riscvphone-src;
+        src = riscvphone-src + "/fw/esp32";
+        dontUseCmakeConfigure = true;
+        dontUseNinjaBuild = true;
         preBuild = ''
-        export CONFIG_LWIP_PPP_SUPPORT=y
-        export CONFIG_LWIP_PPP_NOTIFY_PHASE_SUPPORT=y
-        export CONFIG_LWIP_PPP_PAP_SUPPORT=y
-        export CONFIG_LWIP_PPP_CHAP_SUPPORT=y
+          export CONFIG_LWIP_PPP_SUPPORT=y
+          export CONFIG_LWIP_PPP_NOTIFY_PHASE_SUPPORT=y
+          export CONFIG_LWIP_PPP_PAP_SUPPORT=y
+          export CONFIG_LWIP_PPP_CHAP_SUPPORT=y
+
+          export CONFIG_UNITY_ENABLE_IDF_TEST_RUNNER=false
         '';
         buildInputs = [
 
         ] ++ esp32-toolchain.buildInputs;
+
         nativeBuildInputs = [
 
         ] ++ esp32-toolchain.nativeBuildInputs;
